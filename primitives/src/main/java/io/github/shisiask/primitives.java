cd0c42d22765455c9aca45683c6c8aaf5cf762bf
5hisia <edu.osteen@gmail.com>
2024-12-09 00:38:00 -0500

    java 7 java primitives

---
+++ primitives/src/main/java/io/github/shisiask/primitives.java
@@ -0,0 +1,19 @@
+package io.github.shisiask;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class primitives {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        int a = 2;
+        float b = 6.7f;
+        short c =10;
+        long d = 11;
+        double e = 12.3;
+        boolean f = true;
+        char g = 'a';
+    }
+}
